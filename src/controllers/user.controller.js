25999029ed2b0aed1f772ca5f1f1f8e3ad52f56f
emvalencaf <edsonmvf@gmail.com>
2022-09-08 20:42:51 -0300

    Update user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -53,7 +53,11 @@ export class UserController{
 
             }
 
+            if(!file) return resolve("dist/img/boxed-bg.jpg")
+            
             fileReader.readAsDataURL(file)
+
+            
         })
     }
 
@@ -66,6 +70,8 @@ export class UserController{
         elements.forEach( field => {
 
             if(field.name === "gender" && field.checked) return user[field.name] = field.value
+
+            if(field.name === "admin") return user[field.name] = field.checked
     
             user[field.name] = field.value
     
@@ -86,20 +92,21 @@ export class UserController{
     
     addLine(dataUser){
 
-        this.tableEl.innerHTML = `
-        <tr>
+        const tr = document.createElement('tr')
+
+        tr.innerHTML = `
             <td>
                 <img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm">
             </td>
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
-            <td>${dataUser.admin}</td>
+            <td>${dataUser.admin? 'Sim':'Não'}</td>
             <td>${dataUser.birth}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>
-        </tr>
     `
+        this.tableEl.appendChild(tr)
     }
 }
